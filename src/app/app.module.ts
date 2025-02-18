7bd8d95763b8df1613be2d152954f77fa8ab2253
Simran Maharjan <078bct089.simran@pcampus.edu.np>
2025-02-18 21:20:48 +0545

    inital forntend setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { HomeComponent } from './shared/home/home.component';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
+import { AboutComponent } from './public/about/about.component';
+import { LoginComponent } from './public/login/login.component';
+import { UserComponent } from './private/user/user.component';
+import { TimelineComponent } from './private/timeline/timeline.component';
+import { NavbarComponent } from './public/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    HomeComponent,
+    PublicComponent,
+    PrivateComponent,
+    AboutComponent,
+    LoginComponent,
+    UserComponent,
+    TimelineComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
